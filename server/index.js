a8bceb25cc5dfbc07fb4e0813f8bcbdc76213a77
Ondřej Zahradník <ondrabondy@gmail.com>
2023-11-15 12:20:38 +0100

    Deploying

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+var cors = require('cors')
+require('dotenv').config()
+const colors = require('colors');
+const recipeRoute = require('./routes/recipeRoute')
+const userRoute = require('./routes/userRoute')
+const {connect} = require('./config/db')
+
+app.use(express.json())
+app.use(cors())
+
+//connect db
+connect()
+
+
+//Route for recipes
+app.use('/api/recipes', recipeRoute)
+
+//Route for users
+app.use('/api/users', userRoute)
+
+// Server port
+const PORT = process.env.SERVER_PORT || 8080
+app.listen(PORT, ()=>console.log(`Server running & listening on port: ${PORT}`))
